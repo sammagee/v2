01d96169a9ec5ec1c9b27da7e28e26040c8e4fde
Sam <s@sammagee.me>
2021-08-25 10:19:29 -0500

    updates email

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -17,7 +17,7 @@ export default function Footer() {
               <Tooltip
                 content={
                   <span className="flex items-center space-x-2">
-                    <span>hi@sammagee.me</span>
+                    <span>sam@magee.one</span>
                     <svg className="w-3 h-3 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
                     </svg>
@@ -25,7 +25,7 @@ export default function Footer() {
                 }
                 placement="left"
               >
-                <a className="inline-flex items-center justify-center h-12 px-5 space-x-3 font-semibold transition duration-200 ease-in-out origin-bottom transform border-2 rounded-lg hover:bg-brand-400 focus:bg-brand-400 focus:bg-opacity-10 hover:bg-opacity-10 hover:scale-105 border-brand-400 text-brand-400 active:scale-100 focus:outline-none focus:border-dashed" href="mailto:hi@sammagee.me">
+                <a className="inline-flex items-center justify-center h-12 px-5 space-x-3 font-semibold transition duration-200 ease-in-out origin-bottom transform border-2 rounded-lg hover:bg-brand-400 focus:bg-brand-400 focus:bg-opacity-10 hover:bg-opacity-10 hover:scale-105 border-brand-400 text-brand-400 active:scale-100 focus:outline-none focus:border-dashed" href="mailto:sam@magee.one">
                   <span>Let's Talk</span>
 
                   <svg className="w-4 h-4 transform rotate-90 text-brand-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
