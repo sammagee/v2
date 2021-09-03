318ed2963dd0fb386054b5e4dae79b1d48860187
Sam <s@sammagee.me>
2021-09-03 15:26:42 -0500

    fades dots

--- src/components/dots.tsx
+++ src/components/dots.tsx
@@ -4,7 +4,7 @@ import Image from './image'
 export default function Dots() {
   return (
     <Image
-      className="pointer-events-none select-none transform w-full scale-x-[-1]"
+      className="w-full pointer-events-none select-none"
       containerClassName={isLoaded => clsx('absolute z-0 -right-32 -top-20 w-[32rem] -bottom-20 transition-opacity duration-1000 ease-in', isLoaded ? 'opacity-100' : 'opacity-0')}
       layout="fill"
       objectFit="contain"
